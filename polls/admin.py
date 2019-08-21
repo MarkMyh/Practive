64a96da83675d962329589193980bc2951cfa75c
MYH <15370336773@163.com>
2019-08-21 23:17:13 +0800

    添加后台管理系统添加显示内容

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_time')
+    list_filter = ('pub_time',)#过滤器，结尾需要加逗号（就是添加筛选项）
+    # pass
+admin.site.register(models.Article,ArticleAdmin)
 
--- polls/models.py
+++ polls/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32,default='title')
     content = models.TextField(null=True)
+    pub_time = models.DateTimeField(null=True)#创建的时候就保存当前时间
     def __str__(self):
         return self.title
