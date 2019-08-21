04aa75545341818cc86fd0127cf617473cdfb91f
MYH <15370336773@163.com>
2019-08-21 22:45:00 +0800

    添加博客网页修改

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('index/<int:article_id>/', views.article_page, name='article_page'),
-    path('edit/', views.edit_page, name='edit_page'),
+    path('edit/<int:article_id>/', views.edit_page, name='edit_page'),
     path('edit/action', views.edit_action, name='edit_action'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -16,15 +16,25 @@ def article_page(request, article_id):
     return render(request, 'Article_page.html', {'article': article})
 
 
-def edit_page(request):
+def edit_page(request, article_id):
     # article = models.Article.objects.get(pk=article_id)
-    return render(request, 'Edit_page.html')
+    if str(article_id) == '0':
+        return render(request, 'Edit_page.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'Edit_page.html', {"article":article})
 
 
 def edit_action(request):
     title = request.POST.get('title', 'TITLE')
     content = request.POST.get('content', 'CONTENT')
-    models.Article.objects.create(title=title, content=content)
-    articles = models.Article.objects.all()
-    # return HttpResponse("hello")
-    return render(request, 'index.html', {'articles': articles})
+    article_id = request.POST.get('article_id','0')
+    if article_id == '0':
+        models.Article.objects.create(title=title, content=content)
+        articles = models.Article.objects.all()
+        # return HttpResponse("hello")
+        return render(request, 'index.html', {'articles': articles})
+    article = models.Article.objects.get(pk=article_id)
+    article.title = title
+    article.content = content
+    article.save()
+    return render(request,'Article_page.html', {'article': article})
